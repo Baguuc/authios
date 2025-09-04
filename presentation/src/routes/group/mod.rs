9c4d57d18e7a56a9885da9f41811357fac46e0fe
Jan Żukowski <janzukowski@proton.me>
2025-09-04 20:40:05 +0200

    update: added group's permission granting route

--- presentation/src/routes/group/mod.rs
+++ presentation/src/routes/group/mod.rs
@@ -1,8 +1,10 @@
 pub mod create;
 pub mod delete;
+pub mod grant;
 
 pub fn scope() -> actix_web::Scope {
     actix_web::web::scope("/groups")
         .service(create::controller)
         .service(delete::controller)
+        .service(grant::controller)
 }
