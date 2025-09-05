f53a5bf8893aa840fd9c66dd9b16dd309272d497
Jan Żukowski <janzukowski@proton.me>
2025-09-05 13:39:20 +0200

    update: added user register route

--- presentation/src/routes/user/mod.rs
+++ presentation/src/routes/user/mod.rs
@@ -4,16 +4,25 @@ pub mod authorize;
 pub mod update_pwd;
 pub mod grant_group;
 pub mod revoke_group;
+pub mod register;
 
 pub fn scope() -> actix_web::Scope {
-    actix_web::web::scope("/user")
-        .service(login::controller)
-        .service(actix_web::web::scope("")
-            .guard(actix_web::guard::fn_guard(|ctx| ctx.head().headers().get("authorization").is_some()))
-            .service(info::controller)
-            .service(authorize::controller)
-            .service(update_pwd::controller)
-            .service(grant_group::controller)
-            .service(revoke_group::controller)
+    actix_web::web::scope("")
+        .service(
+            actix_web::web::scope("/user")
+                .service(login::controller)
+        )
+        .service(
+            actix_web::web::scope("/user")
+                .guard(actix_web::guard::fn_guard(|ctx| ctx.head().headers().get("authorization").is_some()))
+                .service(info::controller)
+                .service(authorize::controller)
+                .service(update_pwd::controller)
+                .service(grant_group::controller)
+                .service(revoke_group::controller)
+        )
+        .service(
+            actix_web::web::scope("/users")
+                .service(register::controller)
         )
 }
