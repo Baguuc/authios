10d72a10971f399e37d5c39c4365a8f4aedb6d91
Jan Żukowski <janzukowski@proton.me>
2025-10-11 15:07:59 +0200

    chore: added new routes to scope

--- src/web/routes/user/specific/mod.rs
+++ src/web/routes/user/specific/mod.rs
@@ -7,7 +7,10 @@ pub mod revoke_resource_permission;
 
 pub fn scope() -> actix_web::Scope {
     actix_web::web::scope("/{user_id}")
+        .service(list_resource_permissions::controller)
+        .service(check_resource_permission::controller)
         .service(delete::controller)
+        .service(update::controller)
         .service(grant_resource_permission::controller)
         .service(revoke_resource_permission::controller)
 }
